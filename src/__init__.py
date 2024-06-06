f645437f73540f9418e26cf0fc5b9dd2367d20b7
liiiz <49737596+liiiztw@users.noreply.github.com>
2024-06-06 13:26:36 +0800

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from . import simplified
+from . import traditional
+
+#: A string containing all Simplified characters according to CC-CEDICT.
+simp = simplified = simplified.CHARACTERS
+
+#: A string containing all Traditional characters according to CC-CEDICT.
+trad = traditional = traditional.CHARACTERS
